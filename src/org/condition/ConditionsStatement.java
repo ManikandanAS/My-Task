eddd106273cb0e66e0f48df036fae5dad2990408
manikandanAS <manikandan.s2211@gmail.com>
2021-09-30 11:13:56 +0530

    demo

---
+++ src/org/condition/ConditionsStatement.java
@@ -0,0 +1,24 @@
+package org.condition;
+
+import java.util.Iterator;
+
+public class ConditionsStatement {
+
+	public static void main(String[]args) {
+		
+	
+		for (int i = 1; i < 100; i++) {
+			
+		System.out.println(i);
+		
+			
+			
+			
+			
+	}
+		
+	}
+		
+	}
+	
+
